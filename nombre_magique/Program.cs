eaa91acd4c43f4daf529d1369fb22b1da70bb982
Atilian <gascon.mickael@outlook.fr>
2022-12-24 12:26:01 +0100

    Ajout au repos du programme

---
+++ nombre_magique/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace nombre_magique
+{
+    class program
+    {
+        
+
+        static void Main(String[] args)
+        {
+            game(0, 1, 4);
+        }
+
+
+        static void game(int range1, int range2, int nombreVie)
+        {
+            int magiqueNumber = new Random().Next(range1, range2);
+
+            const string title = "******************\n" +
+                                 "* Nombre magique *\n" +
+                                 "******************\n";
+            
+            Console.WriteLine(title);
+
+            while (nombreVie > 0)
+            {
+                Console.Write("Choose a number beetwen ({0} - {1}) : ", range1, range2);
+
+                int.TryParse(Console.ReadLine(), out var choiceNumber);
+
+                if (magiqueNumber == choiceNumber)   
+                {
+                    Console.WriteLine("You win the number magique is : {0}", magiqueNumber);
+                    return;
+                }
+                else
+                {
+                    if (magiqueNumber > choiceNumber)
+                    {
+                        Console.WriteLine("More\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Less\n");
+                    }
+                }
+
+                nombreVie--;
+            }
+
+            Console.WriteLine("You loose the number magique is : {0}", magiqueNumber);
+        }
+    }
+}
